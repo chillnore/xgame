2ff9a0e865c208d4208e3b5083b46dd19d04941e
hjj2017 <hjj2017@163.com>
2012-07-13 19:28:47 +0800

    【打包工具】 更新打包工具；

---
+++ war2/server/trunk/SceneServer.BizModules/src/war2/sceneServer/bizModules/entry/bizService/EntrySceneResult.java
@@ -0,0 +1,39 @@
+package war2.sceneServer.bizModules.entry.bizService;
+
+/**
+ * 进入场景处理结果
+ * 
+ * @author hjj2019
+ *
+ */
+public class EntrySceneResult {
+	/**
+	 * 获取错误信息
+	 * 
+	 * @return
+	 * 
+	 */
+	public String getError() {
+		return null;
+	}
+
+	/**
+	 * 获取玩家 ID 
+	 * 
+	 * @return
+	 * 
+	 */
+	public String getPlayerID() {
+		return null;
+	}
+
+	/**
+	 * 获取角色 ID 
+	 * 
+	 * @return
+	 * 
+	 */
+	public String getCharacterID() {
+		return null;
+	}
+}
---
+++ war2/server/trunk/SceneServer.BizModules/src/war2/sceneServer/bizModules/entry/bizService/EntrySceneService.java
@@ -0,0 +1,19 @@
+package war2.sceneServer.bizModules.entry.bizService;
+
+/**
+ * 进入场景服务
+ * 
+ * @author hjj2019
+ * 
+ */
+public class EntrySceneService {
+	/**
+	 * 进入场景
+	 * 
+	 * @param ticket
+	 * 
+	 */
+	public EntrySceneResult entryScene(String ticket) {
+		return new EntrySceneResult();
+	}
+}
