af5383bc279e2aca8d727997bace7576993fd1f9
haijiang.jin <hjj2017@163.com>
2012-07-28 21:07:37 +0800

    【游戏服务器】 更新压力测试文件；

--- war2/server/trunk/Common/test.php
+++ war2/server/trunk/Common/test.php
@@ -9,19 +9,29 @@ set_time_limit(0);
 // 创建一个Socket
 $tcp = getprotobyname("tcp");  
 $socket = socket_create(AF_INET, SOCK_STREAM, $tcp);
+
 socket_connect($socket, $host, $port);
+socket_set_block($socket);
 
-// 消息体
-$msg = json_encode(array(
-	"msgTypeID" => 1001, 
-	"msgBody" => array(
-		"ticket" => "123456", 
-	),
-));
+for ($i = 0; $i < 1024; $i++) {
+	// 创建一个随机数
+	$r = rand();
 
-echo "send $msg";
+	// 消息体
+	$msg = json_encode(array(
+		"msgTypeID" => 1001, 
+		"msgBody" => array(
+			"ticket" => "$r", 
+		),
+	));
 
-// 写出消息
-socket_write($socket, $msg, strlen($msg));
+	$msg = "$msg\0";
+	echo "<p>send $msg</p>";
+
+	// 写出消息
+	socket_write($socket, $msg, strlen($msg));
+	sleep(1);
+}
+
 // 关闭 socket
 socket_close($socket);
