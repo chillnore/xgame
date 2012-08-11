d0c3bc6ae6c74be96e71f70a932abe41e318faf6
haijiang.jin <hjj2017@163.com>
2012-08-11 14:30:52 +0800

    【服务器端】 增加消息缓冲；

---
+++ war2/server/trunk/Common/src/war2/common/msg/FlashCrossdomainMsg.java
@@ -0,0 +1,41 @@
+package war2.common.msg;
+
+import java.nio.charset.Charset;
+
+import org.json.JSONObject;
+
+/**
+ * <policy-file-request/> 消息
+ * 
+ * @author hjj2017
+ *
+ */
+public class FlashCrossdomainMsg extends AbstractExternalMsg {
+	/** 消息类型 ID */
+	static final int MSG_TYPE_ID = -2048;
+	/** 消息体 */
+	private static final byte[] MSG_BODY = "<cross-domain-policy><site-control permitted-cross-domain-policies=\"all\" /><allow-access-from domain=\"*\" to-ports=\"*\" /></cross-domain-policy>\0".getBytes(Charset.forName("utf8"));
+
+	@Override
+	public JSONObject serializeToJson() {
+		return null;
+	}
+
+	@Override
+	public void deserializeFromJson(JSONObject jsonObj) {
+	}
+
+	@Override
+	public byte[] serializeToBytes() {
+		return MSG_BODY;
+	}
+
+	@Override
+	public void deserializeFromBytes(byte[] bytes) {
+	}
+
+	@Override
+	public short getMsgTypeID() {
+		return MSG_TYPE_ID;
+	}
+}
